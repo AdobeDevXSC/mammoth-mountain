6becfa7ccc39d8e97d07029b0a206870c0e638a6
KP Parrish <krparrish@adobe.com>
2024-05-17 21:34:21 -0700

    adding code for calendar cards

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -2,18 +2,18 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
   const isIcon = block.classList.contains('icon');
+  const isCalendar = block.classList.contains('calendar')
 
-// un-comment this when adding spreadsheet source
-//   async function fetchJson(link) {
-//     const response = await fetch(link?.href);
-
-//     if (response.ok) {
-//       const jsonData = await response.json();
-//       const data = jsonData?.data;
-//       return data;
-//     }
-//     return 'an error occurred';
-//   }
+  // function for adding spreadsheet json source
+  async function fetchJson(link) {
+    const response = await fetch(link?.href);
+    if (response.ok) {
+      const jsonData = await response.json();
+      const data = jsonData?.data;
+      return data;
+    }
+    return 'an error occurred';
+  }
 
   const ul = document.createElement('ul');
     [...block.children].forEach((row) => {
@@ -22,7 +22,7 @@ export default async function decorate(block) {
       const li = document.createElement('li');
       while (row.firstElementChild) li.append(row.firstElementChild);
       [...li.children].forEach((div) => {
-        if (div.children.length === 1 && div.querySelector('a')) {
+        if (div.children.length === 1 && isIcon && div.querySelector('a')) {
           const linkURL = div.querySelector('a').innerHTML;
           anchor.href = linkURL;
           div.className = 'cards-hide-markdown';
@@ -34,13 +34,38 @@ export default async function decorate(block) {
           div.className = 'cards-card-body';
         }
       });
-	  if (isIcon) {
+	  
+	  if(isIcon) {
 		anchor.append(li);
 		ul.append(anchor);
 	  } else {
 		ul.append(li);
 	  }
     });
+
+	console.log('iscalendar', isCalendar)
+  if(isCalendar) {
+	const link = block.querySelector('a');
+	console.log('link', link)
+	const cardData = await fetchJson(link);
+	console.log('card data', cardData)
+	
+	cardData.forEach((event) => {
+		console.log('event', event)
+		const createdCard = document.createElement('li');
+		createdCard.innerHTML = `
+		<a href="${event.url}" aria-label="${event['anchor-text']}"}">
+			<div class="cards-card-body">
+				<span>
+					<p>${event.dates}</p>
+				</span>
+				<h5>${event.title}</h5>
+			</div>
+		</a>
+		`;
+		ul.append(createdCard);
+	});
+  }
 
   block.textContent = '';
   block.append(ul);
