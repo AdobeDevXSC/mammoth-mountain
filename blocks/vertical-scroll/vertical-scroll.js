6dd2394aea83ee0f6e54d78c6df761c33f5fcfca
KP Parrish <krparrish@adobe.com>
2024-05-22 14:28:12 -0700

    initial vertical scroll styles added

--- blocks/vertical-scroll/vertical-scroll.js
+++ blocks/vertical-scroll/vertical-scroll.js
@@ -3,32 +3,34 @@
  * @param {Element} block The hero block element
  */
 
-
 export default function decorate(block) {
+	const cols = [...block.children];
+	cols.forEach((col) => {
+		col.classList.add('vertical-scroll-item')
+	});
 
-}
+	var items = document.querySelectorAll('.vertical-scroll-item');
+	function isElementInViewport(el) {
+		var rect = el.getBoundingClientRect();
+		return (
+			rect.top >= 0 &&
+			rect.left >= 0 &&
+			rect.bottom <=
+				(window.innerHeight || document.documentElement.clientHeight) &&
+			rect.right <=
+				(window.innerWidth || document.documentElement.clientWidth)
+		);
+	}
 
-// var items = document.querySelectorAll(".timeline-item");
+	function callbackFunc() {
+		for (var i = 0; i < items.length; i++) {
+			if (isElementInViewport(items[i])) {
+				console.log('in-view');
+				items[i].classList.add("in-view");
+			}
+		}
+	}
 
-// function isElementInViewport(el) {
-//     var rect = el.getBoundingClientRect();
-//     return (
-//         rect.top >= 0 &&
-//         rect.left >= 0 &&
-//         rect.bottom <=
-//             (window.innerHeight || document.documentElement.clientHeight) &&
-//         rect.right <=
-//             (window.innerWidth || document.documentElement.clientWidth)
-//     );
-// }
-
-// function callbackFunc() {
-//     for (var i = 0; i < items.length; i++) {
-//         if (isElementInViewport(items[i])) {
-//             items[i].classList.add("in-view");
-//         }
-//     }
-// }
-
-// window.addEventListener("load", callbackFunc);
-// window.addEventListener("scroll", callbackFunc);
+	block.addEventListener("load", callbackFunc);
+	block.addEventListener("scroll", callbackFunc);
+};
