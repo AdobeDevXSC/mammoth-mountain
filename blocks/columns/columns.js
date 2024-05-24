7381582ecec790c1c03ee2c204313d0e182b41a8
KP Parrish <krparrish@adobe.com>
2024-05-23 17:37:21 -0700

    video columns styles updated including responsive

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,13 +1,31 @@
+import { makeVideo } from '../../scripts/scripts.js';
+
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
+
+	  // add video option
+	  if (Object.values(block.classList).includes('video')) {
+		const videoSrc = block.querySelector('div > a');
+
+		if(videoSrc.href.includes(window.hlx.codeBasePath)) {
+		videoSrc.href = videoSrc.text;
+		}
+
+		makeVideo(videoSrc.closest('div'), videoSrc.href);
+		videoSrc.remove();
+
+		const video = block.querySelector('video')
+		const videoWrapper = video.closest('div');
+		videoWrapper.classList.add('video-wrapper');
+	}
 
   // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
-	  const linkedPic = col.querySelector('a picture');
-	  
+	  const linkedPic = col.querySelector('a > picture');
+
 	  if (pic) {
         const picWrapper = pic.closest('div');
         if (picWrapper && picWrapper.children.length === 1) {
@@ -18,7 +36,7 @@ export default function decorate(block) {
 
 	  if (linkedPic) {
         const linkedPicWrapper = pic.closest('div');
-			linkedPicWrapper.className = "linked-image-wrapper"
+		linkedPicWrapper.className = "linked-image-wrapper"
       }
     });
   });
